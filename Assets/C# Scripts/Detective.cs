80c6ab43fb257592080b8406ec6c155ad1833b53
Jerome <jeromeisnthere@gmail.com>
2022-06-02 15:04:24 +0800

    Final Sub

--- Assets/C# Scripts/Detective.cs
+++ Assets/C# Scripts/Detective.cs
@@ -9,46 +9,57 @@ using TMPro;
 
 public class Detective : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-
+    //Display the state/statements of the agent
+    public TextMeshProUGUI detectiveSpeech;
+    //Reference the player gameobject
     public GameObject player;
-
+    //store the clues' transforms into an array
     public Transform[] clues;
-    int _clueIndex = 0;
+    //the index of the clue
+    int currClue = 0;
+    //reference and store the current target's transform
     Transform _currTarget;
+    //find the clue tag to know which objects need to be stored
     public string clueTag;
-
+    //Reference the nav mesh agent to track
     public NavMeshAgent agent;
+    //the dist where the player is close enough to the player
+    public float closeDist;
+    //check if the investigation has started
+    bool igIsStarted;
+    //check if the player is ready
+    bool ready = false;
+    //find the idle pos
+    public Vector3 idlePos;
+    //check if the player has already gotten close to the detective once
+    bool metUp = false;
 
 
     void Start()
     {
+        //find the gameobjects that contain the clue tag and add them to the tranmsform array
         clues = GameObject.FindGameObjectsWithTag(clueTag)
             .Select(go => go.transform)
             .OrderBy(go => go.name)
             .ToArray();
     }
 
-    void Update()
-    {
-
-    }
-
     #region "Tasks"
 
+    //Task to move the agent to the current target
     [Task]
-    void MoveTo(string tag)
+    void GoTo(string tag)
     {
-        if(_currTarget == null)
+        if (_currTarget == null)
         {
             _currTarget = GameObject.FindGameObjectWithTag(tag).transform;
         }
         agent.stoppingDistance = 0.5f;
-        if(tag == "clue")
+        if (tag == "clue")
         {
-            _currTarget = clues[_clueIndex];
+            _currTarget = clues[currClue];
         }
-        if(agent.destination != _currTarget.transform.position)
+        if (agent.destination != _currTarget.transform.position)
         {
             agent.SetDestination(_currTarget.position);
         }
@@ -59,26 +70,125 @@ public class Detective : MonoBehaviour
         }
     }
 
+    //Show that the current state of the agent is idle
     [Task]
     bool Idle()
     {
-        text.text = "Current State: " + "Waiting for player...";
         return true;
     }
 
+    //check if the player is close enough to the agent
     [Task]
     bool isClose(string tag)
     {
         player = GameObject.FindGameObjectWithTag(tag);
-        float distFromPlayer = (agent.transform.position - player.transform.position).x;
-        return Vector3.Distance(player.transform.position, gameObject.transform.position) < distFromPlayer;
+        float distBetween = Vector3.Distance(player.transform.position, agent.destination);
+        return distBetween < closeDist;
     }
 
+    //set to display the dialogue of the agent 
     [Task]
-    void DisplayText(string dispText)
+    void DetectiveLines(string lines)
     {
-        text.text = dispText;
+        this.detectiveSpeech.text = lines;
+        Task.current.Succeed();
     }
 
+    //check if the playuer is ready
+    [Task]
+    bool PlayerReady(bool n)
+    {
+        if(n == true)
+        {
+            ready = true;
+        }
+        else
+        {
+            ready = false;
+        }
+        return true;
+    }
+
+    //set the player to be ready
+    [Task]
+    bool PlayerIsReady()
+    {
+        return ready;
+    }
+
+    //start the investigation 
+    [Task]
+    bool StartInvestigation()
+    {
+        currClue = 0;
+        igIsStarted = true;
+        return true;
+    }
+
+    //if the investigation has been started
+    [Task]
+    bool isInvestigating()
+    {
+        return igIsStarted;
+    }
+
+    //set to end the invesigation 
+    [Task]
+    bool EndInvestigation()
+    {
+        currClue = 0;
+        igIsStarted = false;
+        return true;
+    }
+
+    //check if the player and agent have met once
+    [Task]
+    bool Met()
+    {
+        return metUp;
+    }
+
+    //set if the player has met the agnent
+    [Task]
+    bool haveMet()
+    {
+        metUp = true;
+        return true;
+    }
+
+    //increase the index of the clue and show which clue number it is
+    [Task]
+    bool NoteDown()
+    {
+        detectiveSpeech.text = "Searching Clue: " + (currClue + 1);
+        currClue++;
+        return true;
+    }
+
+    //check which clues are valid to go to
+    [Task]
+    bool ValidClue()
+    {
+        if (currClue < clues.Length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //check to see if all the clues have been found
+    [Task]
+    bool AllCluesFound()
+    {
+        return currClue >= 5;
+    }
     #endregion
+
+    void Update()
+    {
+        Debug.Log(AllCluesFound());
+    }
 }
